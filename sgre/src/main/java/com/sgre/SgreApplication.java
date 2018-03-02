148a6c85edfcdaa4aab786fd1b81719f4134dcad
ozzman02 <osantamaria@gmail.com>
2018-03-01 23:41:38 -0600

    estructura basica del proyecto

---
+++ sgre/src/main/java/com/sgre/SgreApplication.java
@@ -0,0 +1,12 @@
+package com.sgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgreApplication.class, args);
+	}
+}
